73808d60e24fec7010e9b8894938f86c269af871
roi-ri <rodrigo.e.sanchez.araya@gmail.com>
2024-06-03 20:00:49 -0600

    Se agrega el archivo de double_list.h

---
+++ double_list.h
@@ -0,0 +1,28 @@
+#ifndef DOUBLE_LIST_H
+#define DOUBLE_LIST_H
+
+typedef struct Node {
+    in data; 
+    struct Node* prev: 
+    strcut Node* next;
+
+}Node;
+
+typedef struct DoubleList{
+    Node* head;
+    Node* tail;
+} DoubleList;
+
+//Funcion para manejar la lista
+
+DoubleList* create_list();
+void insert_at_beginning(DoubleList* list, int data);
+void insert_at_end(DoubleList* list, int data);
+void insert_at_index(DoubleList* list, int data, int index);
+void delete_by_data(DoubleList* list, int data);
+Node* search_by_data(DoubleList* list, int data);
+void print_list_forward(DoubleList* list);
+void print_list_backward(DoubleList* list);
+void free_list(DoubleList* list);
+
+#endif
