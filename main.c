e9a05084f6dc051c98939581017e414eeca69eff
roi-ri <rodrigo.e.sanchez.araya@gmail.com>
2024-06-03 21:24:27 -0600

    se agrega archivo main.c

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "double_list.h"
+
+int main(){
+    DoubleList* list = create_list();
+
+    insert_at_beggining(list,10);
+    insert_at_beggining(list,20);
+    insert_at_end(list,30);
+    insert_at_index(list,25,2);
+
+
+    printf('Lista hacia adelante:\n');
+    print_list_forward(list);
+
+    printf('Lista hacia atras\n');
+    print_list_backward(list);
+
+    Node* found_node = search_by_data(list, 25);
+    if (found_node != NULL){
+        printf("Nodo encontrado con data 25\n");
+    }else{
+        printf('Nodo no encontrado con data 25\n');
+    }
+
+    delete_by_data(list,20);
+
+    printf('Lista despues de eliminar el nodo con data 20:\n');
+    print_list_forward(list);
+
+    free_list(list);
+
+    return 0;
+
+}
